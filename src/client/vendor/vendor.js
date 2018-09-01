ba927a6911e2f4d1fa5fb2f9cc2b4ae5e2206139
Vinci Rufus <vinci.rufus@gmail.com>
2018-09-02 00:13:40 +0530

    vendor bundles

---
+++ src/client/vendor/vendor.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import '../../../node_modules/bulma/css/bulma.min.css';
+export const React = require('react');
+export const ReactDOM = require('react-dom');
+export const PropTypes = require('prop-types');
+export const classNames = require('classnames');
+/* eslint-enable */
